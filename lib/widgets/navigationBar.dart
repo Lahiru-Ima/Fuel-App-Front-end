90b421a5152e05a2707baee2e103afb79237c44e
Thamasha-ru <thamasharu@gmail.com>
2023-01-04 11:43:27 +0530

    nav bar updated

--- lib/widgets/navigationBar.dart
+++ lib/widgets/navigationBar.dart
@@ -3,6 +3,12 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:fuel_app/theme.dart';
 
+import '../DashBoardPage/productScreen.dart';
+import '../screans/login.dart';
+import '../screans/selectVehicle.dart';
+import '../station_owner_screen/fuelArrivalUpdateScreen.dart';
+import '../userOnQueue/queueUpdateScreen.dart';
+
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
 
@@ -12,16 +18,26 @@ class NavBar extends StatefulWidget {
 
 class _NavBarState extends State<NavBar> {
   int currentIndex = 0;
+  final Screens = [
+    productScreen(),
+    LogInScreen(),
+    QueueUpdate(),
+    SelectVehicle(),
+    FuelArrivalUpdate(),
+
+    // Center(child: Text("Home")),
+    // Center(child: Text("Profile")),
+    // Center(child: Text("Search")),
+    // Center(child: Text("Fuel Availability")),
+    // Center(child: Text("Nearest Fuel Station")),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Home",
-          style: TextStyle(fontSize: 60),
-        ),
-      ),
+      body: Screens[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: kSecondaryColor,
+          iconSize: 30,
           currentIndex: currentIndex,
           onTap: (index) => setState(() => currentIndex = index),
           items: [
