f348a7da55faaacb019e74bf1b75d16037eae6cb
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-06 00:38:03 +0530

    Fetch data to queue screen

--- lib/userOnQueue/queueUpdateScreen.dart
+++ lib/userOnQueue/queueUpdateScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fuel_app/theme.dart';
+import 'package:http/http.dart' as http;
 
 class QueueUpdate extends StatelessWidget {
   const QueueUpdate({super.key});
@@ -7,14 +8,13 @@ class QueueUpdate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         //backgroundColor: const Color.fromARGB(255, 201, 157, 216),
         body: Center(
             child: Column(children: [
       const Image(
         image: AssetImage('images/fuelque.png'),
-        height: 400,
-        width: 300,
+        height: 300,
+        width: 200,
       ),
       Padding(
         padding: const EdgeInsets.all(5.0),
@@ -46,50 +46,72 @@ class QueueUpdate extends StatelessWidget {
             color: kZambeziColor,
             fontSize: 15),
       ),
-      const SizedBox(height: 40),
+      const SizedBox(height: 30),
       Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ElevatedButton(
-            onPressed: (() {}),
-            child: const Text('Join To The Queue'),
+            onPressed: () async {
+              var response = await http.post(
+                  Uri.parse(
+                      "https://fuel-app-backend.up.railway.app/api/fuelQueue"),
+                  body: {
+                    "userName": "lahiru",
+                    "arrivalTime": 8.15.toString(),
+                    "departTime": 9.30.toString(),
+                    "status": "Full"
+                  });
+              print(response.body);
+            },
             style: ElevatedButton.styleFrom(
-              primary: kPrimaryColor,
+              backgroundColor: kPrimaryColor,
               //primary: Colors.purple,
               padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 20),
               textStyle: const TextStyle(
                 fontSize: 20,
                 //fontWeight: FontWeight.bold
               ),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(40.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
             ),
+            child: const Text('Join To The Queue'),
           ),
           const SizedBox(height: 30),
           ElevatedButton(
-            onPressed: (() {}),
-            child: const Text('Exit Before Pump Fuel'),
+            onPressed: () async {
+              var response = await http.post(
+                  Uri.parse(
+                      "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitBeforePump"),
+                  body: {});
+              print(response.body);
+            },
             style: ElevatedButton.styleFrom(
-              primary: kPrimaryColor,
+              backgroundColor: kPrimaryColor,
               // primary: Colors.purple,
               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
               textStyle: const TextStyle(
                 fontSize: 20,
                 //fontWeight: FontWeight.bold
               ),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(40.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
             ),
+            child: const Text('Exit Before Pump Fuel'),
           ),
           const SizedBox(height: 30),
           ElevatedButton(
-            onPressed: (() {}),
-            child: const Text('Exit After Pump Fuel'),
+            onPressed: () async {
+              var response = await http.post(
+                  Uri.parse(
+                      "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitAfterPump"),
+                  body: {});
+              print(response.body);
+            },
             style: ElevatedButton.styleFrom(
-              primary: kPrimaryColor,
+              backgroundColor: kPrimaryColor,
               //primary: Colors.purple,
               padding: const EdgeInsets.symmetric(horizontal: 57, vertical: 20),
               textStyle: const TextStyle(
@@ -97,10 +119,11 @@ class QueueUpdate extends StatelessWidget {
                 fontSize: 20,
                 //fontWeight: FontWeight.bold
               ),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(40.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
             ),
+            child: const Text('Exit After Pump Fuel'),
           ),
         ],
       ),
