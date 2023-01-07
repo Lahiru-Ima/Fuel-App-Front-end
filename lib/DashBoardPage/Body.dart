0b9892723e84bf1f68650c719fc4e43f4a1810f1
Thamasha-ru <thamasharu@gmail.com>
2023-01-07 10:13:13 +0530

    updates

--- lib/DashBoardPage/Body.dart
+++ lib/DashBoardPage/Body.dart
@@ -19,18 +19,18 @@ class Body extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(
-            height: 8,
+            height: 20,
           ),
           ElevatedButton(
             // style: ButtonStyle(backgroundColor: Colors.amber),
             onPressed: () {
               Navigator.pushNamed(context, '/selectNearestShed');
             },
-            child: Text("Search Nearest Fuel station  >"),
+            child: Text("Search Nearest Fuel station"),
             style: ElevatedButton.styleFrom(
               backgroundColor: kSecondaryColor,
               // primary: Colors.purple,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
               textStyle: const TextStyle(
                 fontSize: 20,
                 //fontWeight: FontWeight.bold
@@ -41,18 +41,18 @@ class Body extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 8,
+            height: 20,
           ),
           ElevatedButton(
             // style: ButtonStyle(backgroundColor: Colors.amber),
             onPressed: () {
               Navigator.pushNamed(context, '/joinQueue');
             },
-            child: Text("Join the Queue"),
+            child: Text("Join To The Queue"),
             style: ElevatedButton.styleFrom(
               backgroundColor: kSecondaryColor,
               // primary: Colors.purple,
-              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
               textStyle: const TextStyle(
                 fontSize: 20,
                 //fontWeight: FontWeight.bold
