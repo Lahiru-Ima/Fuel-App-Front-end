b4f900161a39ad5c18a4ba253c563a558a40c2f0
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-15 02:41:15 +0530

    add get data

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:fuel_app/DashBoardPage/Body.dart';
+import 'package:fuel_app/DashBoardPage/get_data.dart';
 import 'package:fuel_app/DashBoardPage/productScreen.dart';
 import 'package:fuel_app/Routings/routes.dart';
 import 'package:fuel_app/screans/customerLogin.dart';
@@ -45,7 +46,7 @@ class FuelApp extends StatelessWidget {
         // home: SearchNearestShed(),
         //home: const LogInScreen(),
         // home: Body(),
-        home: LandingPage(),
+        home: GetData(),
         routes: {
           '/registerPage': (context) => AuthScreen(),
           '/selectNearestShed': (context) => SearchNearestShed(),
