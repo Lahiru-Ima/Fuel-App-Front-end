5fdb0735e29f8b55df5e622b5617f38df46d8cd9
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-15 20:52:48 +0530

    Fix queue errors

--- lib/userOnQueue/queue_details.dart
+++ lib/userOnQueue/queue_details.dart
@@ -20,103 +20,209 @@ class QueueDetails extends StatefulWidget {
 }
 
 class _QueueDetailsState extends State<QueueDetails> {
-  int _clickcount = 0;
+  int _petrolcount = 0;
+  int _dieselcount = 0;
+
+  _QueueDetailsState() {
+    _selectFuelType = _fuelType[0];
+  }
+
+  final _fuelType = [
+    "Diesel",
+    "Petrol",
+  ];
+  String? _selectFuelType = "";
 
   @override
   Widget build(BuildContext context) {
-    // final album = ModalRoute.of(context)!.settings.arguments as Album;
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fuel Queue'),
+        title: const Text('Fuel Queue'),
         backgroundColor: kPrimaryColor,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(widget.dealer),
-            Text(widget.location),
-            Text(widget.petrolStatus),
-            Text(widget.dieselStatus),
-            const SizedBox(height: 20),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _clickcount++;
-                  print(_clickcount);
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kPrimaryColor,
-                //primary: Colors.purple,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 53, vertical: 15),
-                textStyle: const TextStyle(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: 400,
+            height: 180,
+            child: Card(
+              elevation: 0,
+              color: kSecondaryColor,
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                ListTile(
+                  title: Text(
+                    widget.dealer,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  subtitle: Text(widget.location,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w100, fontSize: 18)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Petrol Status = ${widget.petrolStatus}",
+                        style: const TextStyle(fontSize: 15)),
+                    Text("Diesel Status = ${widget.dieselStatus}",
+                        style: const TextStyle(fontSize: 15)),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Queue Type = Petrol",
+                        style: TextStyle(fontSize: 15)),
+                    Text("Queue Count = $_petrolcount",
+                        style: const TextStyle(fontSize: 15)),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Queue Type = Disesl",
+                        style: TextStyle(fontSize: 15)),
+                    Text("Queue Count = $_dieselcount",
+                        style: const TextStyle(fontSize: 15)),
+                  ],
+                )
+              ]),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.fromLTRB(80, 3, 80, 3),
+            decoration: BoxDecoration(
+                color: kPrimaryColor, borderRadius: BorderRadius.circular(40)
+                //.copyWith(bottomRight: Radius.circular(0)),
+                ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                style: const TextStyle(
+                  color: Colors.white, //<-- SEE HERE
                   fontSize: 20,
                   //fontWeight: FontWeight.bold
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.0),
+                dropdownColor: kPrimaryColor,
+                icon: const Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.white, // <-- SEE HERE
                 ),
+                value: _selectFuelType,
+                items: _fuelType
+                    .map((e) => DropdownMenuItem(
+                          child: Text(e),
+                          value: e,
+                        ))
+                    .toList(),
+                onChanged: (val) {
+                  setState(() {
+                    _selectFuelType = val as String;
+                  });
+                },
               ),
-              child: const Text('Join To The Queue'),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                var response = await http.post(
-                    Uri.parse(
-                        "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitBeforePump"),
-                    body: {});
-                // print(response.body);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                // primary: Colors.purple,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                  //fontWeight: FontWeight.bold
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.0),
-                ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                if (_selectFuelType == 'Petrol') {
+                  _petrolcount++;
+                } else {
+                  _dieselcount++;
+                }
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              //primary: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 53, vertical: 15),
+              textStyle: const TextStyle(
+                fontSize: 20,
+                //fontWeight: FontWeight.bold
               ),
-              child: const Text('Exit Before Pump Fuel'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                var response = await http.post(
-                    Uri.parse(
-                        "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitAfterPump"),
-                    body: {});
-                // print(response.body);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                //primary: Colors.purple,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 47, vertical: 15),
-                textStyle: const TextStyle(
-                  color: kTextFieldColor,
-                  fontSize: 20,
-                  //fontWeight: FontWeight.bold
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40.0),
-                ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
               ),
-              child: const Text('Exit After Pump Fuel'),
             ),
-          ],
-        ),
+            child: const Text('Join To The Queue'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: ()
+                // async {
+                //   var response = await http.post(
+                //       Uri.parse(
+                //           "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitBeforePump"),
+                //       body: {});
+                // },
+                {
+              setState(() {
+                if (_selectFuelType == 'Petrol') {
+                  _petrolcount--;
+                } else {
+                  _dieselcount--;
+                }
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              // primary: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              textStyle: const TextStyle(
+                fontSize: 20,
+                //fontWeight: FontWeight.bold
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+            ),
+            child: const Text('Exit Before Pump Fuel'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: ()
+                // async {
+                //   var response = await http.post(
+                //       Uri.parse(
+                //           "https://fuel-app-backend.up.railway.app/api/fuelQueue/exitAfterPump"),
+                //       body: {});
+                //   // print(response.body);
+                // },
+                {
+              setState(() {
+                if (_selectFuelType == 'Petrol') {
+                  _petrolcount--;
+                } else {
+                  _dieselcount--;
+                }
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              //primary: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 47, vertical: 15),
+              textStyle: const TextStyle(
+                color: kTextFieldColor,
+                fontSize: 20,
+                //fontWeight: FontWeight.bold
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+            ),
+            child: const Text('Exit After Pump Fuel'),
+          ),
+        ],
       ),
     );
   }
